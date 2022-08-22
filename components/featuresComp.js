3b4cb99c021c3f10638e0002da76fcc3e0ab06d9
fauxy99 <fauzyhammawa@gmail.com>
2022-08-22 12:12:36 +0100

    red bg

--- components/featuresComp.js
+++ components/featuresComp.js
@@ -57,7 +57,7 @@ export default function FeaturesComp() {
 
       <div className='w-full grid md:grid-cols-3 grid-cols-1 grid-rows-3 gap-3'>
         { featureData.map( (item) => (
-             <div key={item.key} className='border rounded-lg p-3'>
+             <div key={item.key} className='border rounded-lg mb-4 bg-red-300 p-3'>
              <div className='w-full  mb-3'>
                  <img className='w-16' src={item.img} alt={item.key}/>
              </div>
